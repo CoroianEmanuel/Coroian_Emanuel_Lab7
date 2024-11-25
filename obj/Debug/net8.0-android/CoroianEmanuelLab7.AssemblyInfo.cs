3bfa2a7f914bb990606ae796fbd9c886f30e6ea3
unknown <ecoraian@yahoo.com>
2024-11-25 16:21:23 +0200

    Laborator 8 complet

--- obj/Debug/net8.0-android/CoroianEmanuelLab7.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/CoroianEmanuelLab7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoroianEmanuelLab7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d1ae213988986503c30dc96de1a043907ae10cc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoroianEmanuelLab7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoroianEmanuelLab7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
